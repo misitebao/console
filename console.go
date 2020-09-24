2e7620b8f7514164dfb1a0c138a1440a47f00f94
misitebao <i@misitebao.com>
2020-09-24 20:44:14 +0800

    feat:初始化

---
+++ console.go
@@ -0,0 +1,18 @@
+package console
+
+import "github.com/wailsapp/wails/lib/interfaces"
+
+
+
+
+
+// Info 格式化打印变量信息
+func Info(interfaces{}) {
+
+	
+}
+
+// SInfo 返回格式化后的变量信息
+func SInfo(interfaces{}) string {
+
+}
